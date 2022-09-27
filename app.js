22e0272057d08a290e6cb4bec412cba0a31676d3
rossw01 <56947241+rossw01@users.noreply.github.com>
2022-09-27 17:07:58 +0100

    implemented check for each char

--- app.js
+++ app.js
@@ -2,12 +2,40 @@
 
 const API = "https://api.quotable.io/random?maxLength=100&minLength=50";
 const textElement = document.getElementById("quoteDisplay");
+const inputElement = document.getElementById("textInput");
 
 function fetchText() {
   return fetch(API)
     .then((response) => response.json())
     .then((fetchedData) => fetchedData.content);
 }
+
+// DONT SET THIS TO "KEYDOWN"
+inputElement.addEventListener("input", (input) => {
+  let textArray = textElement.querySelectorAll("span"); // Array of all spans (characters we parsed from string)
+  let inputArray = inputElement.value.split("");
+
+  let complete = true; // if this ever gets set to false alone the way, it never gets set back to true
+  textArray.forEach((charSpan, index) => {
+    let inputtedChar = inputArray[index];
+    if (inputtedChar == null) {
+      charSpan.classList.remove("correct");
+      charSpan.classList.remove("typo");
+      complete = false;
+    } else if (inputtedChar == charSpan.innerText) {
+      charSpan.classList.add("correct");
+      charSpan.classList.remove("typo");
+    } else if (inputtedChar != charSpan.innerText) {
+      charSpan.classList.add("typo");
+      charSpan.classList.remove("correct");
+      complete = false;
+    }
+  });
+
+  if (complete) {
+    console.log("complete");
+  }
+});
 
 // Prevents it from being promise call
 async function drawText() {
