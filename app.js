6809f2b1917a85673223c6a2c8a4ca7a8c4c6eb7
rossw01 <56947241+rossw01@users.noreply.github.com>
2022-09-27 15:26:37 +0100

    Divided text to span elements

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 // API :: https://api.quotable.io/random?maxLength=50
 
 const API = "https://api.quotable.io/random?maxLength=100&minLength=50";
+const textElement = document.getElementById("quoteDisplay");
 
-function fetchQuote() {
+function fetchText() {
   return fetch(API)
     .then((response) => response.json())
     .then((fetchedData) => fetchedData.content);
 }
 
 // Prevents it from being promise call
-async function getText() {
-  let text = await fetchQuote();
-  // Inner text allows you to avoid having to type out <p></p>, etc..
-  document.getElementById("quoteDisplay").innerText = text;
-  document.getElementById("quoteInput").value = "";
+async function drawText() {
+  let textToType = await fetchText();
+  textElement.innerText = "";
+
+  textToType.split("").forEach((char) => {
+    let charSpan = document.createElement("span");
+    charSpan.innerText = char;
+    textElement.appendChild(charSpan);
+  });
 }
 
-getText();
+drawText();
